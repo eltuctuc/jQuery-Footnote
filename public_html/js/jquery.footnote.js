fe59cbf981b84cfc5820aea1068cbe791e761b7e
Enrico Reinsdorf <enrico.reinsdorf@re-design.de>
2014-03-15 10:33:56 +0100

    0.0.1

---
+++ public_html/js/jquery.footnote.js
@@ -0,0 +1,34 @@
+(function($) {
+
+	$.fn.footnote = function(options) {
+
+		var settings = $.extend({}, $.fn.footnote.defaults, options);
+
+		return this.each(function(index) {
+			var ol = $('<ol>')
+					.addClass('footnotes-list')
+					.appendTo($(this));
+
+			$('a', $(this)).each(function(index) {
+				var a = $('<a>')
+						.attr('href', '#footnote-' + index)
+						.addClass('footnote')
+						.html(index + 1);
+
+				$('<sup>')
+						.html(a)
+						.insertAfter($(this));
+
+				$(this).after($(this).html());
+
+				$('<li>')
+						.attr('id', 'footnote-' + index)
+						.addClass('footnote-item')
+						.append($(this))
+						.appendTo(ol);
+			});
+		});
+	};
+
+	$.fn.footnote.defaults = {};
+}(jQuery));
